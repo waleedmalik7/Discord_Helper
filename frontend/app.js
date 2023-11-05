5029d3575701da738d92052361afca9eb0f31d60
waleedmalik7 <waleedmalik027@gmail.com>
2023-11-05 03:24:02 -0500

    Full frontend DONE, working on server-side connection, POST request is working for the variables. Need to figure out flow of python-server requests

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,27 @@
 const token = document.querySelector("#token");
 const chID = document.querySelector("#channel");
 const submit = document.querySelector(".submit");
+const baseUrl = 'http://localhost:3000/'
 
-const getInfo = async () =>{
+const postInfo = async () =>{
+    const urlSearchParams = new URLSearchParams();
+
+    // Add key-value pairs to the object
+    urlSearchParams.append("token", token.value);
+    urlSearchParams.append("channel", chID.value);
     
+    // Make an asynchronous POST request using Fetch
+    fetch("/delete", {
+        method: "POST",
+        body: urlSearchParams.toString(),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
 }
 
-submit.addEventListener("submit",()=>{
-
+submit.addEventListener("click",(e)=>{
+    //submit the form
+    e.preventDefault();
+    postInfo();
 })
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000
 
+app.use(express.urlencoded({ extended: true}))
 app.use(express.static('frontend'));
 
 app.get("/",(req,res)=>{
-    
+    res.sendFile(path.join(__dirname, "/frontend/connect.html"));
+})
+
+app.post("/delete",(req,res)=>{
+    console.log(req.body.channel, req.body.token);
 })
 
 app.listen(port, (error)=>{
