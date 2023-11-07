b6a5e76d9ff1c66046fcfdf0a6bcccfda14cf510
waleedmalik7 <waleedmalik027@gmail.com>
2023-11-07 03:57:59 -0500

    added more functions to backend for discord. Need to figure out how to get final message id

--- backend/DiscordApp.py
+++ backend/DiscordApp.py
@@ -3,13 +3,13 @@ import requests
 
 
 #Need to uncomment Below:
-# token = ''
-# channel_id = 
+token = ''
+channel_id = 1
+username = '1'
 
 def sendMessage(token, channel_id, message):
     my_data = {
         "content": message,
-        "tts": True
     }
     my_header = {"authorization": token}
     url = f'https://discord.com/api/v9/channels/{channel_id}/messages'
@@ -32,19 +32,32 @@ def getMessageId(token, channel_id, last_message_id):
     messages = response.json()
     return messages[-1]["id"],messages
 
+def getLatestMessageId(token, channel_id):
+    url = "https://discord.com/api/v9/channels/{}/messages".format(channel_id) 
+    my_header = {"authorization": token}
+    response = requests.get(url=url,headers=my_header)
+    return response.json()[0]['id']
+
+def getUsername(token):
+    url = "https://discord.com/api/v9/users/@me"
+    my_header = {"authorization": token}
+    response = requests.get(url=url,headers=my_header)
+    return response.json()['username']
 
 #Get Message ID's and create a file of all the message IDs with author['id'] == ''
 #Each time we run the messageId we have to identify a start point as a param
 
-#Start with current ID, returns 50 message IDs if author is 'waffleed' and ID of the next start point
+#Start with current ID, returns 50 message IDs if author is 'username' and ID of the next start point
 
 #Need to change currID
+
 currID = 1
 error = False
+
 while(currID != 1):
     currID, messages = getMessageId(token,channel_id,currID)
     for message in messages:
-        if message["author"]["username"] == 'waffleed':
+        if message["author"]["username"] == username:
             print("Deleting:", message["content"])
             sleep(3)
             deleteMessage(token,channel_id,message["id"])
